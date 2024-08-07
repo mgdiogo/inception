5839a5bc1653ddc79a7b3fb4319404ed8713fb4b
Miguel Diogo <miguel.g.diogo@gmail.com>
2024-08-07 19:00:05 +0100

    Added comments to help indicate what is happening in each step of the installation of wordpress, changed theme

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: build up
 
-# path needs to be changed in school, this is my own computer's path
+# Path needs to be changed in school, this is my own computer's path
 
 build:
 		mkdir -p /home/migas/data/mariadb
@@ -13,14 +13,17 @@ up:
 down:
 		docker-compose -f ./srcs/docker-compose.yml down -v
 
-# here we use the logical operator OR to prevent stopping the process if there are any errors (no images to remove, etc)
-# docker system prune -af basically removes ALL unused data from docker
+# Here we use the logical operator OR to prevent stopping the process if there are any errors (no images to remove, etc)
+# Docker system prune -af basically removes ALL unused data from docker
+# !!Remember to remove sudo and fix path in school!!
 
 clean: down
 
-		rm -rf /home/migas/data/mariadb
-		rm -rf /home/migas/data/wordpress
+		sudo rm -rf /home/migas/data/mariadb
+		sudo rm -rf /home/migas/data/wordpress
 		@docker images -q > /dev/null 2>&1 && docker rmi -f $(docker images -qa) || true
 		@docker network ls -q > /dev/null 2>&1 && docker network rm $(docker network ls -q) || true
 		@docker volume ls -q > /dev/null 2>&1 && docker volume rm $(docker volume ls -q) || true
 		docker system prune -af
+
+re: clean build up
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-sleep 5
+sleep 2
+
+# Creates php directory if it does not exist
 
 if [ ! -d /run/php ]; then
     mkdir /run/php
 fi
 
+# If wp-config.php is not found, WordPress will be installed and the said config will be created
 if [ ! -e /var/www/html/wp-config.php ]; then
+
+	printf "\nWordPress configuration will now begin! Don't close this process...\n\n"
+
+	# Updates WordPress command line interface to latest version
     wp cli update --yes --allow-root
 
+	# Downloads WordPress files into /var/www/html since it's our working directory
     wp core download --allow-root
 
+	# Creates wp-config.php and ensures it is stored in the correct path
     wp config create --dbhost=$MYSQL_HOST \
                     --dbname=$MYSQL_DATABASE \
                     --dbuser=$MYSQL_USER \
@@ -18,22 +27,24 @@ if [ ! -e /var/www/html/wp-config.php ]; then
                     --path='/var/www/html' \
                     --allow-root
 
+	# Installs WordPress, sets up website details and creates the admin account
     wp core install --title=$WP_TITLE \
                     --admin_user=$WP_ADMIN \
                     --admin_password=$WP_ADMIN_PASS \
                     --admin_email=$WP_ADMIN_EMAIL \
                     --url=$WP_URL \
                     --allow-root
-
+	# Creates second user, which is only needed due to the subject
     wp user create $WP_USER $WP_USER_EMAIL \
-                    --role=author --user_pass=$WP_USER_PASS \
+                    --user_pass=$WP_USER_PASS \
                     --allow-root
 
-	wp theme install bravada --activate --allow-root
-	wp theme status bravada --allow-root
+	wp theme install astra --activate --allow-root
+	wp theme status astra --allow-root
 	wp option update home "https://${WP_URL}" --allow-root
     wp option update siteurl "https://${WP_URL}" --allow-root
-
+else
+	printf "WordPress configuration is already present, all services are now up and running!\n"
 fi
 
 /usr/sbin/php-fpm7.4 -F
