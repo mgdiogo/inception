ef9060577b1cc60e6ecc4bdd3e457ac0b6bdb7b7
Miguel Diogo <miguel.g.diogo@gmail.com>
2024-08-07 16:44:38 +0100

    Removed unnecessary Admin User creation, this step happens when installing wp in it's own container

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
+# Checks if path exists, if not it means mysql is not installed and begins to install it
+
 if [ ! -d /run/mysqld ]
 then
 
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql
 fi
 
+# If there is no initial script then we will create it, the script creates the database, users, and grants previleges
+
 if [ ! -d init.sql ]
 then
-echo 'Creating the init.sql file.'
+echo 'Data not found... creating initializing script!'
 cat << EOF > init.sql
 	USE mysql;
 	FLUSH PRIVILEGES;
@@ -16,10 +20,7 @@ cat << EOF > init.sql
 	CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 	CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%';
 	SET PASSWORD FOR '$MYSQL_USER'@'%' = PASSWORD('$MYSQL_PASSWORD');
-	CREATE USER IF NOT EXISTS '$WP_ADMIN'@'%';
-	SET PASSWORD FOR '$WP_ADMIN'@'%' = PASSWORD('$WP_ADMIN_PASS');
 	GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'%';
-	GRANT ALL PRIVILEGES ON wordpress.* TO '$WP_ADMIN'@'%';
 	FLUSH PRIVILEGES;
 EOF
 
